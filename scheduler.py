a6c6fa1bd921adab473fe104e2bd55b993652c74
safwan <safwan.mansuri@peak.ai>
2021-04-27 20:27:10 +0530

    change new links

--- scheduler.py
+++ scheduler.py
@@ -8,7 +8,7 @@ print(datetime.datetime.now())
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=20, minute='26')
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=18)
 def scheduled_job():
     startProcess()
 
